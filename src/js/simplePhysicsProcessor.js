8fcd663febad8e457455b11d1a7a4e746b2f69c7
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-08-29 12:38:06 +0200

    Aggiunta rotazione in base ad asse arbitratio prima versione

--- src/js/simplePhysicsProcessor.js
+++ src/js/simplePhysicsProcessor.js
@@ -11,6 +11,9 @@ define([],function () {
 	        	this.entityIndex.set(entity.name , entity);
 	        	this.velocityIndex.set(entity.name , velocity);
 	        	this.rotationIndex.set(entity.name , rotation);
+	        	entity.rotation.x=rotation.ax.x;
+	        	entity.rotation.y=rotation.ax.y;
+	        	entity.rotation.z=rotation.ax.z;
 	        },
 	        process : function (timestamp) {
 	        	//TODO Considerare il timestamp come moltiplicatore/divisore di valori
@@ -22,10 +25,16 @@ define([],function () {
 	        			curEntity.position.y += (curVelocity.y * timestamp);
 	        			curEntity.position.z += (curVelocity.z * timestamp);
 	        		}
-	        		if(!!curRotation && (curRotation.x!==0 || curRotation.y!==0 || curRotation.z!==0)){
-	        			curEntity.rotation.x += (curRotation.x * timestamp);
-	        			curEntity.rotation.y += (curRotation.y * timestamp);
-	        			curEntity.rotation.z += (curRotation.z * timestamp);
+	        		if(!!curRotation && !!curRotation.ax){
+	        			var curQuaternion = curEntity.quaternion;
+	        			var aQuaternion = new THREE.Quaternion();
+	        			var ax = new THREE.Vector3(curRotation.ax.x,curRotation.ax.y,curRotation.ax.z);
+	        			aQuaternion.setFromAxisAngle(ax, curRotation.radians*timestamp);//imposta inizialmente in base all'ora del giorno
+
+	        			curQuaternion.multiplyQuaternions(aQuaternion, curQuaternion);
+	        			curQuaternion.normalize();
+	        			
+	        			curEntity.setRotationFromQuaternion(curQuaternion);
 	        		}
 	        	},this);
 			}
